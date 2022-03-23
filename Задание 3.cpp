7a2b892c6af3e8c035f0c8454b5451a1164c916f
Kenshelent <38864337+Kenshelent@users.noreply.github.com>
2022-03-23 23:45:42 +0600

    Update Задание 3.cpp

--- Задание 3.cpp
+++ Задание 3.cpp
@@ -1,15 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cout << "a = ";
-    cin >> a;
-    cout << "b = ";
-    cin >> b;
-    
-    c = (a + b) * (a + b) + (a - b) * (a - b) - 3*a*b;
-    cout << "c = " << c;
-    return 0;
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	cout << (a + b) * (a + b) + (a - b) * (a - b) - 3 * a * b << endl;
+	return 0;
 }
