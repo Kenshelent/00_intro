b26c0b94e407c8f148ec966c1843dbaa10f96405
Kenshelent <38864337+Kenshelent@users.noreply.github.com>
2022-03-23 23:46:52 +0600

    Update Задание 4.cpp

--- Задание 4.cpp
+++ Задание 4.cpp
@@ -1,15 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+
 
 int main()
 {
-    int a, b, c;
-    cout << "a = ";
-    cin >> a;
-    cout << "b = ";
-    cin >> b;
-
-    c = (a*a - b)* (a*a - b) - 4*a*b*b + 4*(a*a*a - 5)*(a*a*a - 5);
-    cout << "c = " << c;
-    return 0;
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	cout << (a*a-b)* (a*a-b)-4*a*b*b+4*(a*a*a-5)* (a*a*a-5) << endl;
+	return 0;
 }
